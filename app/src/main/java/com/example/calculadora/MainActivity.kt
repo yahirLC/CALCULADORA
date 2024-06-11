e324beb28ad5f1c8205d66078d9a92189874eb14
Yahir Lagunes <yahirlagunesceballos@gmail.com>
2024-06-11 17:28:19 -0600

    Finalizacion de calculadora

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+
 import java.text.DecimalFormat
 import java.time.temporal.IsoFields
 import java.time.temporal.Temporal
@@ -27,6 +30,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var DECIMAL:DecimalFormat
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -37,40 +41,76 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
     fun CAMBIAOPERADOR(b: View){
-        val boton:Button = b as Button
-        if (boton.text.toString().trim() == "x"){
-            OPERACION = "*"
-        }else{
-            OPERACION = boton.text.toString().trim()
-        }
-    }
-    fun CALCULAR(b:View){
-        if (Double.NaN != PRIMERO) {
-            SEGUNDO = tvtemp.text.toString().toDouble()
-            tvtemp.text = ""
-
-            when (OPERACION) {
-                "+" -> PRIMERO = (PRIMERO + SEGUNDO)
-                "-" -> PRIMERO = (PRIMERO - SEGUNDO)
-                "*" -> PRIMERO = (PRIMERO * SEGUNDO)
-                "/" -> PRIMERO = (PRIMERO / SEGUNDO)
-                "%" -> PRIMERO = (PRIMERO % SEGUNDO)
+        if (tvtemp.text.isNotEmpty() || PRIMERO.toString()!= "NaN"){
+            CALCULAR()
+            val boton:Button = b as Button
+            if (boton.text.toString().trim() == "x"){
+                OPERACION = "*"
+            }else{
+                OPERACION = boton.text.toString().trim()
             }
-        }else{
 
-            PRIMERO = tvtemp.text.toString().toDouble()
+            tvresultado.text = DECIMAL.format(PRIMERO) + OPERACION
+            tvtemp.text = ""
         }
 
+    }
+    fun CALCULAR(){
+       try{
+           if (PRIMERO.toString()!="NaN") {
+               if (tvtemp.text.toString().isEmpty()){
+                   tvtemp.text = tvresultado.text.toString()
+               }
+               SEGUNDO = tvtemp.text.toString().toDouble()
+               tvtemp.text = ""
 
+               when (OPERACION) {
+                   "+" -> PRIMERO = (PRIMERO + SEGUNDO)
+                   "-" -> PRIMERO = (PRIMERO - SEGUNDO)
+                   "*" -> PRIMERO = (PRIMERO * SEGUNDO)
+                   "/" -> PRIMERO = (PRIMERO / SEGUNDO)
+                   "%" -> PRIMERO = (PRIMERO % SEGUNDO)
+               }
+           }else{
+
+               PRIMERO = tvtemp.text.toString().toDouble()
+           }
+       }catch (e:Exception){
+
+       }
 
     }
 
     fun SELECCIONARNUM(b: View){
         val boton:Button = b as Button
-        if (tvtemp.text.toString() == "0"){
-            tvtemp.text = ""
-        }
         tvtemp.text = tvtemp.text.toString() + boton.text.toString()
+    }
+
+    fun BORRAR(b: View){
+        val boton:Button = b as Button
+        if (boton.text.toString().trim() == "C"){
+            if(tvtemp.text.toString().isNotEmpty()){
+                var Datos:CharSequence = tvtemp.text as CharSequence
+                tvtemp.text = Datos.subSequence(0,Datos.length-1)
+            }else{
+                PRIMERO = Double.NaN
+                SEGUNDO = Double.NaN
+                tvtemp.text = ""
+                tvresultado.text = ""
+            }
+        }else if (boton.text.toString().trim() == "CA"){
+            PRIMERO = Double.NaN
+            SEGUNDO = Double.NaN
+            tvtemp.text = ""
+            tvresultado.text = ""
+        }
+    }
+
+    fun IGUAL(b:View){
+        CALCULAR()
+        tvresultado.text = DECIMAL.format(PRIMERO)
+        OPERACION = ""
     }
 }
